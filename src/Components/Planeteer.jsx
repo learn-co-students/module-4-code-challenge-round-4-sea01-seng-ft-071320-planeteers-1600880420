f9c52d2120baf5505818b3c87708b8aa65fb8c26
zanhe <zhertica@gmail.com>
2020-09-23 12:01:54 -0700

    cc mod4

--- src/Components/Planeteer.jsx
+++ src/Components/Planeteer.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 
 class Planeteer extends React.Component {
 
+  state = {
+    
+    showDetails: false
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      showDetails: !prevState.showDetails
+    }))
+  }
+
   render() {
+    // console.log(this.state)
     return (
       <li className="cards__item">
         <div className="card">
-          <img src={"RENDER IMAGE"} alt={"RENDER PERSON NAME"} className="card__image" />
+          <img src={this.props.planeteer.pictureUrl} alt={this.props.planeteer.name} className="card__image" onClick={this.handleClick} />
           <div className="card__content">
-            <div className="card__title">{"RENDER NAME"}</div>
-            <p className="card__text">{"CONDITIONALLY RENDER BIO OR QUOTE"}</p>
+            <div className="card__title">{this.props.planeteer.name}</div>
+            <p className="card__text"> {this.state.showDetails? this.props.planeteer.quote :this.props.planeteer.bio } </p>
             <div className="card__detail">
-              <p>{"RENDER TWITTER HANDLE"}</p>
-              <p>{"CONDITIONALLY RENDER WHETHER THE PERSON IS USA-BASED OR WORKING OVERSEAS"}</p>
+              <p>{this.props.planeteer.twitter}</p>
+              <p>{this.props.planeteer.fromUSA? "USA-based":"Working Overseas"}</p>
             </div>
           </div>
         </div>
@@ -21,5 +33,13 @@ class Planeteer extends React.Component {
   }
 
 }
+// bio: "Xiye Bastida is a teenage climate activist currently based in New York City and one of the lead organizers of the Fridays For Future youth climate strike movement. Bastida was born and raised in Mexico as part of the Otomi-Toltec indigenous peoples. She sits on the administration committee of the Peoples Climate Movement, where she brings the voice of youth to existing grassroots and climate organizations. She received the “Spirit of the UN” award in 2018."
+// born: 2002
+// fromUSA: true
+// id: 3
+// name: "Xiye Bastida"
+// pictureUrl: "https://160g7a3snajg2i1r662yjd5r-wpengine.netdna-ssl.com/wp-content/uploads/2019/12/xiya.jpg"
+// quote: "We are on Earth to take care of life. We are on Earth to take care of each other."
+// twitter:
 
 export default Planeteer;
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+  handleChange = (e) => {
+    e.preventDefault()
+    console.log(e.target.value)
+
+    // if props.planteere.name/.bio includes what is tyedin search then display the card, orhterwise render all
+
+    let currentList = []
+    let newList = []
+    if (e.target.value !== "") {
+      currentList = this.props.planeteer.map(planeteer => planeteer)
+      newList = currentList.filter(planeteer=> {
+        const lc = planeteer.name.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lc.includes(filter)
+      })
+    } else {
+      newList = this.props.planeteer
+    }
+    // https://dev.to/iam_timsmith/lets-build-a-search-bar-in-react-120j
+  }
+
   render() {
     return (
       <div className="search">
+        <div>
+          <input type="text" className="input" onChange={this.handleChange} placeholder="Who Would You Like To Search For?" />
+        </div>
       </div>
     );
   }
